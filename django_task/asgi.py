c1168a521fd9580eb4ceabb6822f09f4feba249b
vinnu000 <vinodvinnu000@gmail.com>
2022-11-08 21:41:41 +0530

    root app name changed

--- django_task/asgi.py
+++ django_task/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dvara_task project.
+ASGI config for django_task project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dvara_task.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_task.settings')
 
 application = get_asgi_application()
--- django_task/urls.py
+++ django_task/urls.py
@@ -1,4 +1,4 @@
-"""dvara_task URL Configuration
+"""django_task URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
