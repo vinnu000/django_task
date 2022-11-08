6d4935dfd1f96bb0e0986d38713615f4102e2505
vinnu000 <vinodvinnu000@gmail.com>
2022-11-08 22:24:52 +0530

    settings file changes

--- django_task/settings.py
+++ django_task/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Message Tags to define there level.
 MESSAGE_TAGS = {
